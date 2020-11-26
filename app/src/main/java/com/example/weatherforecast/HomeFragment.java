eb72a6a1002ea86f375306cf3a5f7ec44c5614a5
phong016688 <duongnguyet.bm@gmail.com>
2020-11-26 20:46:59 +0700

    nguyet

--- app/src/main/java/com/example/weatherforecast/HomeFragment.java
+++ app/src/main/java/com/example/weatherforecast/HomeFragment.java
@@ -1,64 +1,79 @@
 package com.example.weatherforecast;
 
+import android.animation.AnimatorInflater;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView timeTextView;
+    private TextView statusTextView;
+    private ImageView statusImageView;
+    private TextView temperatureTextView;
+    private TextView temperatureUpTextView;
+    private TextView temperatureDownTextView;
 
     public HomeFragment() {
-        // Required empty public constructor
+        super(R.layout.fragment_home);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        timeTextView = view.findViewById(R.id.time_text_view);
+        statusTextView = view.findViewById(R.id.status_text_view);
+        statusImageView = view.findViewById(R.id.icon_status_image_view);
+        temperatureTextView = view.findViewById(R.id.temperature_text_view);
+        temperatureUpTextView = view.findViewById(R.id.temperature_up_text_view);
+        temperatureDownTextView = view.findViewById(R.id.temperature_down_text_view);
+        fakeData1();
+        setupAnim();
+    }
+
+    private void fakeData1() {
+        DateFormat dateFormat = new SimpleDateFormat("E, HH:mm dd:MM:yyyy", new Locale("vi"));
+        timeTextView.setText(dateFormat.format(new Date(System.currentTimeMillis())));
+        statusTextView.setText("Nắng nhẹ");
+        statusImageView.setImageResource(R.drawable.ic_sun);
+        temperatureTextView.setText("28");
+        temperatureUpTextView.setText("25°");
+        temperatureDownTextView.setText("30°");
+    }
+
+    private void setupAnim() {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(
+                statusImageView,
+                "translationY",
+                statusImageView.getY() - 10, statusImageView.getY() + 10);
+        animator.setDuration(2000);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.setRepeatMode(ValueAnimator.REVERSE);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.start();
     }
 }
--- app/src/main/java/com/example/weatherforecast/MainActivity.java
+++ app/src/main/java/com/example/weatherforecast/MainActivity.java
@@ -14,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container_fragment, HomeFragment.newInstance(), HomeFragment.class.getSimpleName())
+                .commit();
     }
 }
